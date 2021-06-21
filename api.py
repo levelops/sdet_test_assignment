7efda178d3205ce91f681b7777751430594ef0cf
Gershon Shif <76446666+gershon-levelops@users.noreply.github.com>
2021-06-21 16:07:55 -0700

    Create api.py

---
+++ api.py
@@ -0,0 +1,16 @@
+# wrapper function for /stations
+# wrapper function for /datasets
+# wrapper function for /datatypes
+
+from setup_logger import logger
+import pytest
+
+
+def get_stations():
+  pass
+
+def get_datasets():
+  pass
+
+def get_datatypes():
+  pass
